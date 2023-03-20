7be9357a04d82239151f9ba6d3c39116b8602735
ijazdev101 <ijazkhan19932@gmail.com>
2023-03-20 22:51:18 +0500

    About us
    About us

--- imdb/src/app/about/page.jsx
+++ imdb/src/app/about/page.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div className='max-w-6xl mx-auto space-x-4'> 
-        <h1 className='text-2xl font-medium text-amber-600'>About </h1>
-        <p>
-        IMDb (an acronym for Internet Movie Database)[2] is an online database of information related to films, television series, podcasts, home videos, video games, and streaming content online – including cast, production crew and personal biographies, plot summaries, trivia, ratings, and fan and critical reviews. IMDb began as a fan-operated movie database on the Usenet group "rec.arts.movies" in 1990, and moved to the Web in 1993. Since 1998, it is now owned and operated by IMDb.com, Inc., a subsidiary of Amazon.
-
-As of March 2022, the database contained some 10.1 million titles (including television episodes) and 11.5 million person records.[3] Additionally, the site had 83 million registered users. The site's message boards were disabled in February 2017.
-
-As of February 12, 2023, IMDb is the 54th most visited website in the world, according to website ranker Alexa.[4]
-        </p>
+    <div className="max-w-6xl mx-auto space-x-4">
+      <h1 className="text-2xl font-medium text-amber-600 ml-5 mt-6 mb-7">
+        About IMDb
+      </h1>
+      <p className="mt-5 ">
+        IMDb (an acronym for Internet Movie Database)[2] is an online database
+        of information related to films, television series, podcasts, home
+        videos, video games, and streaming content online – including cast,
+        production crew and personal biographies, plot summaries, trivia,
+        ratings, and fan and critical reviews. IMDb began as a fan-operated
+        movie database on the Usenet group "rec.arts.movies" in 1990, and moved
+        to the Web in 1993. Since 1998, it is now owned and operated by
+        IMDb.com, Inc., a subsidiary of Amazon.
+      </p>
+      <p className="mt-6">
+        As of March 2022, the database contained some 10.1 million titles
+        (including television episodes) and 11.5 million person records.[3]
+        Additionally, the site had 83 million registered users. The site's
+        message boards were disabled in February 2017.
+      </p>
+      <p className="mt-5 ">
+        As of February 12, 2023, IMDb is the 54th most visited website in the
+        world, according to website ranker Alexa.[4]
+      </p>
     </div>
-  )
+  );
 }
