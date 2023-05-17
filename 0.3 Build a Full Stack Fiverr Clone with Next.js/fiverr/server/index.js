8b2a2436599766a3c7991f87a218bbc5484ab967
ijazdev101 <ijazkhan19932@gmail.com>
2023-05-17 23:47:56 +0500

    app
    app

--- 0.3 Build a Full Stack Fiverr Clone with Next.js/fiverr/server/index.js
+++ 0.3 Build a Full Stack Fiverr Clone with Next.js/fiverr/server/index.js
@@ -1,11 +1,19 @@
-import  express from 'express'
-import dotenv from "dotenv"
-
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(cros({ origin: [process.env.PUBLIC_URL] ,
+method:["GET","POST","PUT","PATCH","DELETE"],
+credentials:true,
+}));
+
+
+app.use(express.json());
+
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
