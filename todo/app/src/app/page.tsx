d3dc43b139d235fec274332f862b7e0c19f88374
ijazdev101 <ijazkhan19932@gmail.com>
2023-05-20 11:25:28 +0500

    TODO next js
    TODO next js

---
+++ todo/app/src/app/page.tsx
@@ -0,0 +1,12 @@
+
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+<div>
+  <h1>Next js TODO APP</h1>
+</div>
+  )
+}
