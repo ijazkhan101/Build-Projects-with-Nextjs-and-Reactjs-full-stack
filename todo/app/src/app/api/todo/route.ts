330cb45bff5d1807f014f230c4940db4226a92a6
ijazdev101 <ijazkhan19932@gmail.com>
2023-05-22 23:18:39 +0500

    Make a Contact Form in Next.js 13 & TS
    Make a Contact Form in Next.js 13 & TS

--- todo/app/src/app/api/todo/route.ts
+++ todo/app/src/app/api/todo/route.ts
@@ -1,6 +1,29 @@
-import {db} from "@vercel/postgres";
-import { NextRequest , NextResponse} from "next/server"
+import { db } from "@vercel/postgres";
+import { NextRequest, NextResponse } from "next/server";
 
-export  function GET(request: NextRequest) {
-  return  NextResponse.json({message:'Hello, Next.js!'})
+export async function GET(request: NextRequest) {
+  const client = await db.connect();
+  try {
+    await client.sql`CREATE TABLE IF NOT EXISTS Todos(id serial, Task varchar(255) );`;
+    const res = await client.sql`SELECT * from Todos`
+    return    NextResponse.json({data:res })
+  } catch (err) {
+    console.log(err);
+    return  NextResponse.json({message:"Something went wrong"});
+  }
+}
+
+export async function POST(request: NextRequest){
+  const client = await db.connect();
+  const req = await request.json();
+  try{
+    if(req.task){
+      const res = await client.sql`INSERT INTO Todos(Task) VALUES(${req.task})`
+      return NextResponse.json({message:"Data Added Successfully"})
+    }else{
+      throw new Error("Task field is required")
+    }
+  } catch(error){
+    return NextResponse.json({message:(error as{ message:string}).message});
+  }
 }
