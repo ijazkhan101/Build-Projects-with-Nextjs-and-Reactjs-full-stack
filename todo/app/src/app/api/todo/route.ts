5d34ec9e634789ad517406b4393cd63ca5069b17
ijazdev101 <ijazkhan19932@gmail.com>
2023-06-03 10:52:31 +0500

    push
    push

--- todo/app/src/app/api/todo/route.ts
+++ todo/app/src/app/api/todo/route.ts
@@ -1,33 +1,19 @@
 import { QueryResult } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
-import {Todo ,NewTodo,db, todoTable} from "../../lib/drizzle"
+import { Todo, NewTodo, db, todoTable } from "../../lib/drizzle";
 import { sql } from "@vercel/postgres";
+
 export async function GET(request: NextRequest) {
-  const client = await db.connect();
   try {
     await sql`CREATE TABLE IF NOT EXISTS Todos(id serial, Task varchar(255) );`;
-    
-    const res = await db.select().from(todoTable).execute()
 
+    const res = await db.select().from(todoTable);
 
-    return    NextResponse.json({data:res })
+    return NextResponse.json({ data: res });
   } catch (err) {
+    console.log((err as { message: string }).message);
     console.log(err);
-    return  NextResponse.json({message:"Something went wrong"});
+    return NextResponse.json({ message: "Something went wrong" });
   }
 }
 
-export async function POST(request: NextRequest){
-  const client = await db.connect();
-  const req = await request.json();
-  try{
-    if(req.task){
-      const res = await sql`INSERT INTO Todos(Task) VALUES(${req.task})`
-      return NextResponse.json({message:"Data Added Successfully"})
-    }else{
-      throw new Error("Task field is required")
-    }
-  } catch(error){
-    return NextResponse.json({message:(error as{ message:string}).message});
-  }
-}
--- todo/app/src/app/lib/drizzle.ts
+++ todo/app/src/app/lib/drizzle.ts
@@ -8,9 +8,9 @@ import { drizzle} from "drizzle-orm/vercel-postgres";
 import {InferModel} from  "drizzle-orm";
 import { sql } from "@vercel/postgres";
 
-export const todoTable = pgTable("Todos",{
+export const todoTable = pgTable("todos",{
     id: serial("id").primaryKey(),
-    task : varchar("Task", { length: 255}).notNull()
+    task : varchar("task", { length: 255}).notNull()
 })
 
 export type Todo =  InferModel<typeof todoTable>;
@@ -18,8 +18,3 @@ export type Todo =  InferModel<typeof todoTable>;
 export type NewTodo =  InferModel<typeof todoTable, 'insert'>;
 
 export const db  = drizzle(sql);
-
-db.insert(todoTable).values({
-    task : "ijaz"
-})
-
