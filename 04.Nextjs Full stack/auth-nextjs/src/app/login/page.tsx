93d55edf4164af35d9e52e02237c1bfa892ae95f
ijazdev101 <ijazkhan19932@gmail.com>
2023-09-07 17:35:18 +0500

    push
    push

---
+++ 04.Nextjs Full stack/auth-nextjs/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoginPage() {
+  return (
+    <div className="flex">
+      <h1 className="text-center  text-2xl">Login</h1>
+    </div>
+  );
+}
+
+export default LoginPage;
