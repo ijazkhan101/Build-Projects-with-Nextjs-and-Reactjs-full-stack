6c6dc51e8a1b3268f3925f5e0e92e018a6b456a4
ijazdev101 <ijazkhan19932@gmail.com>
2023-12-09 20:25:13 +0500

    car website
    car website

--- next01/lib/getAllUsers.tsx
+++
@@ -1,8 +0,0 @@
-
-export default async function getAllUsers(){
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
-
-    if (!res.ok) throw new Error('Failed to fetch data ')
-        return res.json()
-
-}
