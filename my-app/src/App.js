5862d7a250a5d3a69d484790805239f8bcd2127e
ijazdev101 <ijazkhan19932@gmail.com>
2024-01-09 20:16:40 +0500

    reactjs
    reactjs

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React in 2024 as i am learning you from 2019 haha</h1>
+    </div>
+  );
+}
+
+export default App;
